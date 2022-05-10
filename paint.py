45e6f9c67c998457ad7c64a9e64788e697021baa
Santiago Quihui <santiagoquihui@hotmail.com>
2022-05-10 12:26:30 -0500

    Finish circle function

--- paint.py
+++ paint.py
@@ -36,9 +36,15 @@ def square(start, end):
     end_fill()
 
 
-def circle(start, end):
+def draw_circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    circle(end.x-start.x)
+    end_fill()
 
 
 def rectangle(start, end):
@@ -81,7 +87,7 @@ onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', draw_circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
