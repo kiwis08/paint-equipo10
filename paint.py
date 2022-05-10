19455fa3aac5d77a8e840d056c86c0f3141fba23
MJOTec <105307387+MJOTec@users.noreply.github.com>
2022-05-10 12:48:24 -0500

    rectangle

--- paint.py
+++ paint.py
@@ -43,7 +43,19 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x - start.x)/2)
+        left(90)
+
+    end_fill()
+    
 
 
 def triangle(start, end):
