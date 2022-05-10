cc471687c30becaf2b77de5466bad376fb226a67
MJOTec <105307387+MJOTec@users.noreply.github.com>
2022-05-10 12:48:04 -0500

    triangle

--- paint.py
+++ paint.py
@@ -48,7 +48,16 @@ def rectangle(start, end):
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(110)
+
+    end_fill()
 
 
 def tap(x, y):
